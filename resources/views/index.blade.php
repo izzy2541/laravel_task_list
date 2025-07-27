a73e32648ae6cb4bf6400bce53d8592a38358467
izzy2541 <isabel.donnelly@hotmail.com>
2025-07-27 17:27:02 +1000

    Blade directives

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,15 @@
-Hi im
-The name is {{$name}}
+<h1>
+    The list of tasks
+</h1>
+
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->title]) }}">{{ $task ->title}}</a>
+        </div>
+     {{-- instead of else, we can use @empty which means run the below code if the
+        task list is empty --}}
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+</div>
