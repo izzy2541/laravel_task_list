287460dfcd30ea07e2b651ae27add88fe262bdfc
izzy2541 <isabel.donnelly@hotmail.com>
2025-07-30 20:44:03 +1000

    Keeping old values in the form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -12,7 +12,7 @@
 
 
 @section('content')
-{{$errors}}
+{{-- {{$errors}} --}}
     <form method="POST" action="{{ route('tasks.store') }}">
         {{-- csrf is a middleware that protects users from malicious websote requests --}}
         @csrf
@@ -23,13 +23,13 @@
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
-            <input text="text" name="title" id="title"/>
+            <input text="text" name="title" id="title" value="{{ old('title')}}"/>
         </div>
         <div>
             <label for="Description">
                 Description
             </label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <textarea name="description" id="description" rows="5">{{ old('description')}}</textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -38,7 +38,7 @@
             <label for="long_description">
                Long Description
             </label>
-            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description')}}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
