f1a1623c8f69850f426cb8d3e149937ebd81faf4
izzy2541 <isabel.donnelly@hotmail.com>
2025-07-30 10:08:48 +1000

    Model and reading data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,16 +2,12 @@
 
 @section('title', 'The list of tasks')
 
-
 @section('content')
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['id' => $task->title]) }}">{{ $task ->title}}</a>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
         </div>
-     {{-- instead of else, we can use @empty which means run the below code if the
-        task list is empty --}}
     @empty
         <div>There are no tasks!</div>
     @endforelse
 @endsection
-
--- routes/web.php
+++ routes/web.php
@@ -1,81 +1,25 @@
 <?php
 
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
 
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
-Route::get('/', function (){
+Route::get('/', function () {
     return redirect()->route("tasks.index");
- });
+});
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-     'tasks' => $tasks
+        'tasks' => \App\Models\Task::latest()->get()
     ]);
- })->name('tasks.index');
+})->name('tasks.index');
 
- Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    //converts arrays to a laravel collection object, which lets you call methods
-    $task = collect($tasks)->firstWhere('id', $id);
-
-    if(!$task){
-        abort(Response::HTTP_NOT_FOUND);
-    }
-
-    return view('show', ['task' => $task]);
- })->name('tasks.show');
+Route::get('/tasks/{id}', function ($id) {
+    //find is an inbuilt method which lets you fetch a row from the database
+    return view('show', [
+        //find or fail will return 404 page if nothing can be found
+        'task' => \App\Models\Task::findOrFail($id)
+    ]);
+})->name('tasks.show');
 
 
 // Route::get('/xxx', function () {
@@ -93,5 +37,3 @@ Route::get('/tasks', function () use ($tasks) {
 // Route::fallback(function () {
 //     return "Still got somewhere!";
 // });
-
-
