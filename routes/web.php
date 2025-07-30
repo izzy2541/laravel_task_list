73cb2ac30cbdfd51ad16ffd371ec7ebe514d5c3b
izzy2541 <isabel.donnelly@hotmail.com>
2025-07-30 20:27:00 +1000

    Edit form

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Route::get('/tasks', function () {
 Route::view('tasks/create', 'create')
 ->name('tasks.create');
 
+Route::get('/tasks/{id}/edit', function ($id) {
+    //find is an inbuilt method which lets you fetch a row from the database
+    return view('edit', [
+        //find or fail will return 404 page if nothing can be found
+        'task' => Task::findOrFail($id)
+    ]);
+})->name('tasks.edit');
+
 Route::get('/tasks/{id}', function ($id) {
     //find is an inbuilt method which lets you fetch a row from the database
     return view('show', [
@@ -50,7 +58,25 @@ Route::post('/tasks', function (Request $request) {
     ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:225',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
 
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+    // "with" method lets you set session data
+    //here we're setting variable called "success" which we parse into app.blade.php
+    //is a flash temporary message to show message after redirect (task saved)
+    ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
 // Route::get('/xxx', function () {
 //     return "Hello";
 // })->name('hello');
