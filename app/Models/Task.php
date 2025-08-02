02d72fa0c9f3dee9865105b242f61c38547c8603
izzy2541 <isabel.donnelly@hotmail.com>
2025-08-02 20:31:23 +1000

    Toggle task state

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,4 +12,9 @@ class Task extends Model
     //fillable is more secure.
     protected $fillable = ['title', 'description', 'long_description'];
     // protected $guarded = ['secret'];
+
+    public function toggleComplete(){
+        $this->completed = !$this->completed;
+        $this->save();
+    }
 }
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -14,9 +14,30 @@
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
 
+    <p>
+        @if ($task->completed)
+            Completed
+        @else
+            Not completed
+        @endif
+    </p>
+    <div>
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+    </div>
+
+    <div>
+        <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task])}}">
+            @csrf
+            @method('PUT')
+            <button type="submit">
+                Mark as {{ $task->completed ? 'not completed' : 'completed'}}
+            </button>
+        </form>
+    </div>
+
     <div>
         {{-- builds form as /tasks/3 for example --}}
-        <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
+        <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
             {{-- generates a CSRF token for protection against cross-site request forgery. --}}
         @csrf
         {{-- tells Laravel to treat this POST request as a DELETE request
